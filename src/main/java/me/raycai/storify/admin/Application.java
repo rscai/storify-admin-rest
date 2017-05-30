3107af4ed64455a602f199f76cab7214fec4416a
Ray Cai <kkppccdd@gmail.com>
2017-05-30 17:38:40 +0800

    #TECH add cors configuration, allow *.

--- src/main/java/me/raycai/storify/admin/Application.java
+++ src/main/java/me/raycai/storify/admin/Application.java
@@ -2,11 +2,27 @@ package me.raycai.storify.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @SpringBootApplication
 public class Application {
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
