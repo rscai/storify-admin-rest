a06efd591984ccc55fe39bc3487a334f1ad1d227
Ray Cai <kkppccdd@gmail.com>
2017-06-01 22:03:17 +0800

    #TECH exposed id for all JPA entities on rest API.

--- src/main/java/me/raycai/storify/admin/Application.java
+++ src/main/java/me/raycai/storify/admin/Application.java
@@ -1,12 +1,18 @@
 package me.raycai.storify.admin;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import javax.persistence.EntityManager;
 
 @Configuration
 @SpringBootApplication
@@ -21,6 +27,24 @@ public class Application {
         corsConfiguration.addAllowedMethod("*");
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
         return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Bean
+    public RepositoryRestConfigurer repositoryRestConfigurer() {
+
+        return new RepositoryRestConfigurerAdapter() {
+            @Override
+            public void configureRepositoryRestConfiguration(
+                    RepositoryRestConfiguration config) {
+                entityManager.getMetamodel()
+                        .getEntities()
+                        .stream()
+                        .forEach(entity -> config.exposeIdsFor(entity.getJavaType()));
+            }
+        };
     }
 
     public static void main(String[] args) {
